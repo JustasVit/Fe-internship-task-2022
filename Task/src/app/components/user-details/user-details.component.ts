f52f1c49e84d212063f3478ec5469cdc6259c064
Justas Vitkauskas <justas.vitkauskas@visma.com>
2022-07-27 16:29:58 +0300

    Added translation to Lithuanian language

--- Task/src/app/components/user-details/user-details.component.ts
+++ Task/src/app/components/user-details/user-details.component.ts
@@ -7,21 +7,21 @@ import { DatePipe } from '@angular/common';
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss']
 })
-export class UserDetailsComponent implements OnInit {
+export class UserDetailsComponent {
 
   loggedInUser: User =
     {name:"Justas",
     surname:"Vitkauskas",
-    dateOfBirth:new Date("2000-04-01"),
+    dateOfBirth: new Date("2000-04-01"),
     gender:"Male",
     country:"Lithuania",
     city:"Vilnius",
     hobbies:"-"}
 
-  pipe = new DatePipe('en-US');
-  dateString : string | null = null;
-
-  ngOnInit(): void {
-    this.dateString = this.pipe.transform(this.loggedInUser.dateOfBirth, 'dd/MM/yyyy');
-  }
+  // pipe = new DatePipe('en-US');
+  // dateString : string | null = null;
+  //
+  // ngOnInit(): void {
+  //   this.dateString = this.pipe.transform(this.loggedInUser.dateOfBirth, 'dd/MM/yyyy');
+  // }
 }
